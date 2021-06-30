fa950834f42085b78e6c01452118a91d795a4c89
Sujith <sujithtom@gmail.com>
2021-07-01 00:52:55 +0100

    first commit

---
+++ product-service/src/main/java/com/tomji/core/product/productservice/services/ProductServiceImpl.java
@@ -0,0 +1,32 @@
+package com.tomji.core.product.productservice.services;
+
+import com.tomji.api.core.product.Product;
+import com.tomji.api.core.product.ProductService;
+import com.tomji.util.exceptions.InvalidInputException;
+import com.tomji.util.exceptions.NotFoundException;
+import com.tomji.util.http.ServiceUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductServiceImpl implements ProductService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
+    private final ServiceUtil serviceUtil;
+
+    public ProductServiceImpl(ServiceUtil serviceUtil) {
+        this.serviceUtil = serviceUtil;
+    }
+
+    @Override
+    public Product getProduct(int productId) {
+        LOG.debug("/product return the found product for productId={}", productId);
+
+        if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
+
+        if (productId == 13) throw new NotFoundException("No product found for productId: " + productId);
+
+        return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
+    }
+}
