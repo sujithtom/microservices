a11201fb66f1cdaaf783d34802873bd7d5bb49fd
Tom <sujithtom@gmail.com>
2021-07-05 00:33:22 +0100

    Added Product Composite service

--- product-composite-service/src/main/java/com/tomji/core/product/productcompositeservice/ProductCompositeServiceApplication.java
+++ product-composite-service/src/main/java/com/tomji/core/product/productcompositeservice/ProductCompositeServiceApplication.java
@@ -2,12 +2,21 @@ package com.tomji.core.product.productcompositeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@ComponentScan("com.tomji")
 public class ProductCompositeServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductCompositeServiceApplication.class, args);
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
