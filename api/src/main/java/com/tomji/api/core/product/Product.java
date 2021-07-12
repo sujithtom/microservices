492eab1ede2640ed710bfd8a4cf99d55c53567f2
Sujith <sujithtom@gmail.com>
2021-07-12 23:01:54 +0100

    Added docker files

--- api/src/main/java/com/tomji/api/core/product/Product.java
+++ api/src/main/java/com/tomji/api/core/product/Product.java
@@ -6,6 +6,14 @@ public class Product {
     private final int weight;
     private final String serviceAddress;
 
+    public Product() {
+        productId = 0;
+        name = null;
+        weight = 0;
+        serviceAddress = null;
+
+    }
+
     public Product(int productId, String name, int weight, String serviceAddress) {
         this.productId = productId;
         this.name = name;
