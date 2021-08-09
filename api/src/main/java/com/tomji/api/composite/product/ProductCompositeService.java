c2b224869d87fe7e39be61dd6f5f8658b8098b93
Sujith <sujithtom@gmail.com>
2021-08-10 00:49:34 +0100

    Added persistance to composite api

--- api/src/main/java/com/tomji/api/composite/product/ProductCompositeService.java
+++ api/src/main/java/com/tomji/api/composite/product/ProductCompositeService.java
@@ -1,11 +1,36 @@
 package com.tomji.api.composite.product;
 
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.web.bind.annotation.*;
 
 @Api(description = "REST API for composite product information.")
 public interface ProductCompositeService {
+
+    @ApiOperation(
+            value = "${api.product-composite.create-composite-product.description}",
+            notes = "${api.product-composite.create-composite-product.notes}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad Request, invalid format of the request. See response message for more information."),
+            @ApiResponse(code = 422, message = "Unprocessable entity, input parameters caused the processing to fail. See response message for more information.")
+    })
+    @PostMapping(
+            value    = "/product-composite",
+            consumes = "application/json")
+    void createCompositeProduct(@RequestBody ProductAggregate body);
+
+
+    @ApiOperation(
+            value = "${api.product-composite.delete-composite-product.description}",
+            notes = "${api.product-composite.delete-composite-product.notes}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad Request, invalid format of the request. See response message for more information."),
+            @ApiResponse(code = 422, message = "Unprocessable entity, input parameters caused the processing to fail. See response message for more information.")
+    })
+    @DeleteMapping(value = "/product-composite/{productId}")
+    void deleteCompositeProduct(@PathVariable int productId);
 
     /**
      * Sample usage: curl $HOST:$PORT/product-composite/1
@@ -13,8 +38,18 @@ public interface ProductCompositeService {
      * @param productId
      * @return the composite product info, if found, else null
      */
+    @ApiOperation(
+            value = "${api.product-composite.get-composite-product.description}",
+            notes = "${api.product-composite.get-composite-product.notes}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad Request, invalid format of the request. See response message for more information."),
+            @ApiResponse(code = 404, message = "Not found, the specified id does not exist."),
+            @ApiResponse(code = 422, message = "Unprocessable entity, input parameters caused the processing to fail. See response message for more information.")
+    })
     @GetMapping(
-            value = "/product-composite/{productId}",
+            value    = "/product-composite/{productId}",
             produces = "application/json")
-    ProductAggregate getProduct(@PathVariable int productId);
+    ProductAggregate getCompositeProduct(@PathVariable int productId);
+
+
 }
